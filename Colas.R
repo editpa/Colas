22515d39a89e7ff8fd35ca814b5799c5beac56fa
editpa <editpa@localhost.localdomain>
2023-10-19 09:24:57 -0600

    Commit de tarea

---
+++ Colas.R
@@ -0,0 +1,8 @@
+# Correr los siguientes comandos, comentar la linea y subir un archivo con su nombre
+cola <- list()
+cola <- c(cola, "Dato 1")
+print(cola)
+
+Dato_eliminado <- cola[1]
+cola <- cola[-1]
+print(cola)
